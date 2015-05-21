0152522d26913e90544ef004e09a032ae1310983
Rafal2228 <rafal2228@gmail.com>
2015-05-22 00:08:49 +0200

    Favicon, Modal, Logo link
    Lot of changes. Modal appears after clicking map, need to test button
    opacity on all browsers, favicon created for all possible use cases, logo
    is now the same link as 'onas'

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(function() {
-    var wayNames = ['wayOnas', 'wayOferta', 'waySzkolenia', 'waySklep', 'wayKontakt'];
-    var navBarHeight = $('#MenuNavBar').outerHeight();
+    var wayNames = [$('#wayOnas'), $('#wayOferta'), $('#waySzkolenia'), $('#waySklep'), $('#wayKontakt')];
+    var navBarHeight = $('#MenuNavBar');
     var wayPoints = [];
 
     $.scrollIt({
@@ -10,29 +10,48 @@ $(function() {
         scrollTime: 600,       // how long (in ms) the animation takes
         activeClass: 'active', // class given to the active nav element
         onPageChange: null,    // function(pageIndex) that is called when page is changed
-        topOffset: -navBarHeight           // offste (in px) for fixed top navigation
+        topOffset: -navBarHeight.outerHeight()           // offste (in px) for fixed top navigation
     });
 
     var assignWayPoints = function(){
-        for(var i = 0 ; i < wayNames.length - 1 ; i++){
-            wayPoints[i] = $('#' + wayNames[i]).waypoint(function(direction){
-                console.log(this.element.id + ' hit')
+        for(var i = 0 ; i < wayNames.length ; i++){
+            wayPoints[i] = wayNames[i].waypoint(function(direction){
+                var tabs = $(this.element).find('.trigger');
+                if((this.key !== 'waypoint-1')&&(this.key !== 'waypoint-4')) {
+                    if(tabs !== null){
+                        tabs.removeClass('trigger');
+                        tabs.addClass('animated');
+                    }
+                }
+                else {
+                    var i = 0;                     //  set your counter to 1
+
+                    function myLoop () {           //  create a loop function
+                        setTimeout(function () {    //  call a 3s setTimeout when the loop is called
+                            $(tabs[i]).removeClass('trigger');
+                            $(tabs[i]).addClass('animated');          //  your code here
+                            i++;                     //  increment the counter
+                            if (i < tabs.length) {            //  if the counter < 10, call the loop function
+                                myLoop();             //  ..  again which will trigger another
+                            }                        //  ..  setTimeout()
+                        }, 250)
+                    }
+
+                    myLoop();                      //  start the loop
+                }
             }, {
                 offset: function(){
-                    return navBarHeight;
+                    var winHeight = 2* $(window).height() /3;
+                    return navBarHeight.outerHeight() + winHeight;
                 }
             });
         };
-        wayPoints[4] = $('#' + wayNames[i]).waypoint(function(direction){
-            console.log(this.element.id + ' hit')
-        }, {
-            offset: 'bottom-in-view'
-        });
     };
     assignWayPoints();
 
+
+
     $(window).resize(function(){
-        navBarHeight = $('#MenuNavBar').outerHeight();
         Waypoint.refreshAll();
     });
 });
