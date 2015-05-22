293f4134d3777976dfd22bdf1991534358c0cbfe
Rafal2228 <rafal2228@gmail.com>
2015-05-22 21:44:02 +0200

    Modal Map implementation, autocomplete

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ $(function() {
     var navBarHeight = $('#MenuNavBar');
     var wayPoints = [];
 
+    //Init scrolling
     $.scrollIt({
         upKey: 38,             // key code to navigate to the next section
         downKey: 40,           // key code to navigate to the previous section
@@ -13,6 +14,7 @@ $(function() {
         topOffset: -navBarHeight.outerHeight()           // offste (in px) for fixed top navigation
     });
 
+    //Assigning waypoints
     var assignWayPoints = function(){
         for(var i = 0 ; i < wayNames.length ; i++){
             wayPoints[i] = wayNames[i].waypoint(function(direction){
@@ -49,9 +51,63 @@ $(function() {
     };
     assignWayPoints();
 
-
-
     $(window).resize(function(){
         Waypoint.refreshAll();
+    });
+
+    //Google maps stuff
+    var directionsDisplay;
+    var directionsService = new google.maps.DirectionsService();
+    var map;
+    var mapInitialized = false;
+    var tasLatlng = new google.maps.LatLng(53.152593, 16.690242);
+    var inputField = document.getElementById('mapDirectionsStart');
+    var autocomplete;
+    var locationPrediction;
+
+    function initialize() {
+        directionsDisplay = new google.maps.DirectionsRenderer();
+        autocomplete = new google.maps.places.Autocomplete(inputField, {
+            country: 'pl'
+        });
+        google.maps.event.addListener(autocomplete, 'place_changed', function() {
+            locationPrediction = autocomplete.getPlace();
+        });
+        var mapOptions = {
+            zoom: 16,
+            center: tasLatlng,
+            disableDefaultUI: true
+        };
+        map = new google.maps.Map(document.getElementById("mapContent"), mapOptions);
+        var markerTas = new google.maps.Marker({
+            position: tasLatlng,
+            map: map,
+            title: 'G³ówna siedziba TAS'
+        });
+        directionsDisplay.setMap(map);
+        mapInitialized = true;
+    }
+    function calcRoute() {
+        var start = inputField.value;
+        var end = tasLatlng;
+        var request = {
+            origin:start,
+            destination:end,
+            travelMode: google.maps.TravelMode.DRIVING
+        };
+        directionsService.route(request, function(response, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                directionsDisplay.setDirections(response);
+            }
+        });
+    }
+
+    $("#mapModal").on('shown.bs.modal', function(){
+        if(!mapInitialized) initialize();
+    });
+    $("#mapDirectionsForm").on('submit', function(e){
+        e.preventDefault();
+        calcRoute();
+        return false;
     });
 });
